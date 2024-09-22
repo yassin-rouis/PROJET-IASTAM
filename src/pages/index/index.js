a013d553fab7e9c2998ab70edf83ae4b0a1d41c1
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-09-22 23:56:30 +0100

    fc

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,27 @@
+import {Viewer} from "@xeokit/xeokit-sdk/src/viewer/Viewer.js";
+import {XKTLoaderPlugin} from "@xeokit/xeokit-sdk/src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js";
+import {useEffect} from "react";
+
+
 export default function Index() {
-    return <></>
+    useEffect(() => {
+        const viewer = new Viewer({
+            canvasId: "viewerContainer"
+        });
+
+        const xktLoader = new XKTLoaderPlugin(viewer);
+
+        xktLoader.load({
+            src: "./data/haus.ifc.xkt"
+        }).then(() => {
+            console.log("BIM model loaded successfully!");
+        });
+    }, []);
+
+    return <div>
+        <canvas id="viewerContainer" style={{
+            width: '100%',
+            height: '600px',
+        }}></canvas>
+    </div>
 }
